971799d62c0b796bee384ccce6b06cfd9661b5c0
anya.shan <anya.shanaida@yahoo.com>
2023-02-22 19:40:27 +0200

    add io.socket

--- myapp/chat/server.js
+++ myapp/chat/server.js
@@ -6,7 +6,32 @@ const io = require("socket.io")(server);
 
 server.listen(process.env.PORT || 3000, function () {
   console.log("Server running in port 3000");
+  // http://localhost:3000/
   //   console.log(__dirname + "/public");
 });
 
 app.use(express.static(__dirname + "/public"));
+
+const users = {};
+
+io.sockets.on("connection", (client) => {
+  const broadcast = (event, data) => {
+    client.emit(event, data);
+    client.broadcast.emit(event, data);
+  };
+
+  broadcast("user", users);
+
+  client.on("message", (message) => {
+    if (users[client.id] !== message.name) {
+      users[client.id] = message.name;
+      broadcast("user", users);
+    }
+    broadcast("message", message);
+  });
+
+  client.on("disconnect", () => {
+    delete users[client.id];
+    client.broadcast.emit("user", users);
+  });
+});
