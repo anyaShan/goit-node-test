c34b7cb85e36f02ae86d33281a3171bcd9f33743
anya.shan <anya.shanaida@yahoo.com>
2023-02-22 19:07:13 +0200

    add chat

---
+++ myapp/chat/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+server.listen(process.env.PORT || 3000, function () {
+  console.log("Server running in port 3000");
+  //   console.log(__dirname + "/public");
+});
+
+app.use(express.static(__dirname + "/public"));
